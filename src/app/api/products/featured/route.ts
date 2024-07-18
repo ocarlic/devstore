fef238498ae6b03ba8d00c8cf43740ac5e262ad8
Victor Carli <victorcarlibr@gmail.com>
2024-07-17 23:34:48 -0300

    feat: create route handlers

---
+++ src/app/api/products/featured/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+import data from "../data.json";
+
+export async function GET() {
+  const featuredProducts = data.products.filter((product) => product.featured);
+
+  return NextResponse.json(featuredProducts);
+}
